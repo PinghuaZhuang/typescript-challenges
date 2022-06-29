3f18ca874d12aabb45d38c0182ae7b86a9d29368
PinghuaZhuang <zphua2016@gmail.com>
2022-06-29 15:24:57 +0800

    Update Conact.ts

--- challenges/Conact.ts
+++ challenges/Conact.ts
@@ -1,6 +1,8 @@
-type Concat<T extends unknown[], U extends unknown[]> = [keyof T, keyof U];
+// 这里不能使用 keyof, keyof 不是数组
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
 
-type Result = Concat<[1], [2]> // expected to be [1, 2]
+type Result = Concat<[4], [5]> // expected to be [1, 2]
 
-const retConcat: Result = [1, 2];
-// const retConcat: Result = [1]; // error
+
+const retConcat: Result = [4, 5]; // error
+// const retConcat: Result = [1, 2]; // error
