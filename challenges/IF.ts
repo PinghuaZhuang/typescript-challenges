d2ba7c8f66931d0f5970b36a9797262b254d3804
PinghuaZhuang <zphua2016@gmail.com>
2022-06-29 10:52:58 +0800

    Update IF.ts

--- challenges/IF.ts
+++ challenges/IF.ts
@@ -1,7 +1,16 @@
-type If<T, A, B> = T extends boolean ? (T extends true ? A : B) : never;
+type If<T extends boolean, A, B> = T extends true ? A : B;
 
 type IFA = If<true, 'a', 'b'>  // expected to be 'a'
 type IFB = If<false, 'a', 'b'> // expected to be 'b'
 
 const retA: IFA = 'a';
 // const retB: IFB = 'a'; // error
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+type ExcludeType1 = string | number | { name: string };
+
+const retExclude1: MyExclude<ExcludeType1, number | string> = {
+  name: 'string',
+}
+// const retExclude2: MyExclude<ExcludeType1, number> = 1111; error
