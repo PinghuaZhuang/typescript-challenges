bf4cf98aab94ba5cd5d746a1e7a80627cf325a01
PinghuaZhuang <zphua2016@gmail.com>
2022-06-30 14:42:57 +0800

    Update Omit.ts

--- built-in/Omit.ts
+++ built-in/Omit.ts
@@ -29,11 +29,17 @@ type MyExclude1<T, U> = T extends U ? never : T;
 type MyOmit1<T, U> = {
   [P in keyof T]: P extends U ? never : T[P];
 };
+// HARD:
 type MyOmit2<T, U> = {
-  // 为啥这么写不行???
-  // 类似与匿名函数自执行
-  [P in (keyof T extends U ? never : keyof T)]: T[P]
+  // 这么写是不对的
+  // 不能加括号,
+  // [P in (keyof T extends U ? never : keyof T)]: T[P]
+  // [P in keyof T as P extends U ? never : P]: T[P]
+
+  // 在行内, in 和 infer 都可以声明泛型
+  [P in keyof T as P extends U ? never : P]: T[P]
 };
+
 type MyOmit<T, U> = {
   [P in MyExclude1<keyof T, U>]: T[P]
 };
@@ -41,4 +47,3 @@ type MyOmit<T, U> = {
 type test1 = ('description' | 'title') extends 'title' ? true : false // false
 type test2 = 'title' extends ('description' | 'title') ? true : false // true
 type test3 = Todo extends { title: string } ? true : false;
-//
