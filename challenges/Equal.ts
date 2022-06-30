718df6d4542e418da81362426e2f33d43d0bc09b
PinghuaZhuang <zphua2016@gmail.com>
2022-06-30 14:42:59 +0800

    Update Equal.ts

--- challenges/Equal.ts
+++ challenges/Equal.ts
@@ -1,4 +1,5 @@
-type Equal1<X, Y> =  (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
+// HARD:
+type Equal1<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
 
 // readonly 不能被 extends 区分
 type eqret1 = Equal1<{ name: string }, { readonly name: string }>; // false
